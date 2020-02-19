69985677473ee0ada26f41fdaf0b115eb1db48b7
Ryan Hardin <ryanwhardin@gmail.com>
2020-02-19 14:44:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist,artistName,songTitle) {
   return playlist
 }
 
-function removeFromPlaylist() {
+function removeFromPlaylist(playlist,artistName) {
   delete playlist['artist']
   return playlist
 }

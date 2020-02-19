0fc0c801249df5442e5185fe80b40a9bb7bfa8e1
Ryan Hardin <ryanwhardin@gmail.com>
2020-02-19 14:45:19 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist(playlist,artistName,songTitle) {
 }
 
 function removeFromPlaylist(playlist,artistName) {
-  delete playlist['artist']
+  delete playlist[artist:'My Bloody Valentine']
   return playlist
 }
